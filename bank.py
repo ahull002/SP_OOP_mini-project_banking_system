39cee2cbf670f1f83b592d47e55f53fe0f83f04e
Alfred Hull <alfred.d.hull@gmail.com>
2020-11-27 20:55:23 -0500

    Add files via upload

---
+++ bank.py
@@ -0,0 +1,15 @@
+class Bank:
+
+    name = 'Springboard Federal Credit Union'
+    clients = []
+
+    def update_db(self, client):
+        self.clients.append(client)
+
+    def authentication(self, name, account_number):
+        for i in range(len(self.clients)):
+            if name in self.clients[i].account.values() and account_number in self.clients[i].account.values():
+                print()
+                print("Authentication successful!")
+                print("========================================================================================")
+                return self.clients[i]
